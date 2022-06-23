5085eb023acbe5651ee650a5d5c27aa9ed772d00
StijnGyssens <stijn.gyssens@gmail.com>
2022-06-23 12:19:58 +0200

    testen cookie

--- components/forms/loginform.jsx
+++ components/forms/loginform.jsx
@@ -16,15 +16,15 @@ export default function Loginform() {
   } = useForm();
 
   const onSubmit = async (data) => {
-    /* const {
+    const {
       data: { token },
     } = await instance.post(`/login_check`, data);
-    console.log(token); */
-    const response = await fetch(
+    console.log(token);
+    /* const response = await fetch(
       `${process.env.NEXT_PUBLIC_BASEPATH}/login_check`,
       { method: "POST", credentials: "include", body: JSON.stringify(data) }
     );
-    console.log(response);
+    console.log(response); */
   };
 
   console.log(errors);
