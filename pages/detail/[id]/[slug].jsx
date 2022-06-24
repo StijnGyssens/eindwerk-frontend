0af1a9c8c66339547c06544e7824d16776cc30b1
StijnGyssens <stijn.gyssens@gmail.com>
2022-06-24 22:45:14 +0200

    change form group v1

--- pages/detail/[id]/[slug].jsx
+++ pages/detail/[id]/[slug].jsx
@@ -13,47 +13,129 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import Loginform from "../../../components/forms/loginform";
+import { useContext } from "react";
+import { userContext } from "../../_app";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import { Input, Select, Textarea } from "@chakra-ui/react";
 
-const Detail = ({ id, group }) => {
+const Detail = ({ id, group, fight, region, timeperiode }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    userid: { value, change },
+  } = useContext(userContext);
+
+  const fights = fight["hydra:member"];
+  const regions = region["hydra:member"];
+  const timeperiodes = timeperiode["hydra:member"];
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    data = JSON.stringify(data);
+    axios
+      .patch(`${process.env.NEXT_PUBLIC_BASEPATH}/groups${id}`, data, {
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/ld+json",
+        },
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+        router.push("/list");
+      })
+      .catch((error) => console.error(error));
+  };
+  console.log(errors);
 
   return (
     <Layout>
-      <p>name {group.name}</p>
-      <p>description: {group.description}</p>
-      {group.events.length > 0 && (
+      {!value && (
         <div>
-          Events:
-          {group.events.map()}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              type="text"
+              placeholder="Name"
+              {...register("name", { required: true })}
+            />
+            <Input
+              type="text"
+              placeholder="location"
+              {...register("location", { required: true })}
+            />
+            <Textarea {...register("description", {})} />
+            <Select {...register("fightingStyle")}>
+              <option value="">none</option>
+              {fights.map((f) => (
+                <option key={f["@id"]} value={f["@id"]}>
+                  {f.fightingStyle}
+                </option>
+              ))}
+            </Select>
+            <Select {...register("historicalRegion", { required: true })}>
+              {regions.map((r) => (
+                <option key={r["@id"]} value={r["@id"]}>
+                  {r.historicalRegion}
+                </option>
+              ))}
+            </Select>
+            <Select {...register("timeperiode", { required: true })}>
+              {timeperiodes.map((t) => (
+                <option key={t["@id"]} value={t["@id"]}>
+                  {t.timeperiode}
+                </option>
+              ))}
+            </Select>
+
+            <Input type="submit" />
+          </form>
         </div>
       )}
-      <div>Fighting style: {group.fightingStyle.fightingStyle}</div>
-      <div>Region: {group.historicalRegion.historicalRegion}</div>
-      {group.members.length > 0 && (
+      {value && (
         <div>
-          <p>Members:</p>
-          {group.members.map((group) => (
-            <p key={group["@id"]}>
-              {group.firstName} {group.lastName}
-            </p>
-          ))}
+          <p>name {group.name}</p>
+          <p>description: {group.description}</p>
+          {group.events.length > 0 && (
+            <div>
+              Events:
+              {group.events.map()}
+            </div>
+          )}
+          <div>Fighting style: {group.fightingStyle.fightingStyle}</div>
+          <div>Region: {group.historicalRegion.historicalRegion}</div>
+          {group.members.length > 0 && (
+            <div>
+              <p>Members:</p>
+              {group.members.map((group) => (
+                <p key={group["@id"]}>
+                  {group.firstName} {group.lastName}
+                </p>
+              ))}
+            </div>
+          )}
+          <p>timeperiode: {group.timeperiode.timeperiode}</p>
+          <Button onClick={onOpen}>login</Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>log in</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Loginform onClose={onClose} />
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={onClose}>close</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </div>
       )}
-      <p>timeperiode: {group.timeperiode.timeperiode}</p>
-      <Button onClick={onOpen}>login</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>log in</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Loginform onClose={onClose} />
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>close</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
     </Layout>
   );
 };
@@ -77,8 +159,17 @@ export const getStaticProps = async (context) => {
   const { data: group } = await axios.get(
     `${process.env.NEXT_PUBLIC_BASEPATH}/groups/${id}`
   );
+  const { data: fight } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASEPATH}/styles`
+  );
+  const { data: region } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASEPATH}/regions`
+  );
+  const { data: timeperiode } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASEPATH}/timeperiodes`
+  );
   return {
-    props: { id, group },
+    props: { id, group, fight, region, timeperiode },
     revalidate: 10,
   };
 };
