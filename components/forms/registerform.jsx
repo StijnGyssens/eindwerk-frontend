da59f729dfaa91ae1dbdbda11da5409640625729
StijnGyssens <stijn.gyssens@gmail.com>
2022-06-25 17:02:23 +0200

    aanpassen register

--- components/forms/registerform.jsx
+++ components/forms/registerform.jsx
@@ -1,4 +1,16 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 export default function Register() {
   const {
@@ -6,34 +18,66 @@ export default function Register() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const onSubmit = (data) => {
+    data.groups = [data.groups];
+    data = JSON.stringify(data);
+    console.log(data);
+    axios.post(`${process.env.NEXT_PUBLIC_BASEPATH}/members`, data, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/ld+json",
+      },
+      withCredentials: true,
+    });
+    onClose();
+  };
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="email"
-        placeholder="Email"
-        {...register("Email", { required: true })}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        {...register("password", { required: true })}
-      />
-      <input
-        type="text"
-        placeholder="firstName"
-        {...register("firstName", { required: true })}
-      />
-      <input
-        type="text"
-        placeholder="lastName"
-        {...register("lastName", { required: true })}
-      />
-      <input type="checkbox" placeholder="leader" {...register("leader", {})} />
+    <>
+      <Button onClick={onOpen}>Register</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Register</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="email"
+                placeholder="email"
+                {...register("email", { required: true })}
+              />
+              <input
+                type="password"
+                placeholder="password"
+                {...register("simplePassword", { required: true })}
+              />
+              <input
+                type="text"
+                placeholder="firstName"
+                {...register("firstName", { required: true })}
+              />
+              <input
+                type="text"
+                placeholder="lastName"
+                {...register("lastName", { required: true })}
+              />
+              <input
+                type="checkbox"
+                placeholder="leader"
+                {...register("leader", {})}
+              />
 
-      <input type="submit" />
-    </form>
+              <input type="submit" />
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
